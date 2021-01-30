fd0b4abbabfccb67eb6efb96b4d8d756a93dc12f
ccsxw6 <ccsxw6@gmail.com>
2021-01-30 17:47:53 -0600

    removing comments

--- app.js
+++ app.js
@@ -7,21 +7,15 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-// GIVING PATHS TO SEND DATA TO? 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-// C:\Users\cstanfi2\Downloads\Bootcamp\template\HTML_Template_Engine\output
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-// C:\Users\cstanfi2\Downloads\Bootcamp\template\HTML_Template_Engine\output\team.html
 
-// allowing use of htmlRenderer
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
 const render = require("./lib/htmlRenderer");
 
-// Array to hold employee objects
 const employees = []
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 const promptUser = () =>
 {    inquirer.prompt([
         {
@@ -33,7 +27,6 @@ const promptUser = () =>
     ]).then((answers) => {
         console.log(answers)
         if (answers.type == 'Finished') {
-            // If Team is complete, call function to build team and write HTML file
             makeTeam()
             
         }else if (answers.type == 'Engineer') {
@@ -70,9 +63,6 @@ const  manager = () =>
             message: 'What is their Office Number?',
         },
     ]).then((answers) => {
-        // creating a new Manager class and setting it to variable newManager w/ the users responses
-        // then pushing that variable to the employees array
-        // getting Manager class from Manager.js
      const newManager =   new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
      employees.push(newManager)
      promptUser()
@@ -140,36 +130,12 @@ const engineer = () =>
 promptUser()
 
 
-// Writes the HTML Created to the output folder with file name team.html
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 function makeTeam() {
     fs.writeFileSync(outputPath, render(employees))
-    // render function comes from htmlRenderer - 
-    //employees = array of all employee objects
-    // calling render(employees), then taking that data and writing it to outputPath
-    // outputPath = C:\Users\cstanfi2\Downloads\Bootcamp\template\HTML_Template_Engine\output\team.html
-    // SO it's sending that information to the above path
-    // console.log(employees)
     console.log('Successfully wrote to team.html and placed it in the output folder!')
 }
 
 
 
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
